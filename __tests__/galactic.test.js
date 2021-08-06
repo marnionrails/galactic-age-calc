cdef24109db3e1179c238916def1f82c6c29d4e0
marnionrails <37088724+marnionrails@users.noreply.github.com>
2021-08-06 00:30:06 -0400

    record good fail for instantiating  Galactic object with numerical argument

---
+++ __tests__/galactic.test.js
@@ -0,0 +1,10 @@
+import Galactic from './../src/galactic.js';
+
+describe('Galactic', () => {
+
+  test('should create an instance of Galactic constructor with integer as age property', () => {
+  let user_age = "not a num";
+  let galactic = new Galactic(user_age);
+  expect(user_age).toEqual(Number.isInteger(user_age));
+  });
+});
