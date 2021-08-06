0e167947f84a596c7912d7bb6c6f5e3bce85a702
marnionrails <37088724+marnionrails@users.noreply.github.com>
2021-08-06 01:25:53 -0400

    pass test where isNumber() returns string asking for number if other type is entered

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -3,7 +3,8 @@ import Galactic from './../src/galactic.js';
 describe('Galactic', () => {
 
   test('should return a string asking for a number, given any other type of input is entered', () => {
-    const userAge = new Galactic(6);
+    const user_age = "not a num";
+    const userAge = new Galactic(user_age);
     userAge.isNumber();
     const result = userAge.dialogue;
     expect(result).toEqual("please enter a valid number for age");  
